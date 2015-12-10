b805aa3b585afd2c11de5f78c9954fed24278435
monchewharry <caodingxian@gmail.com>
2015-12-10 14:16:52 -0600

    hwk3

--- Regression/hwk5_601/hwk5_601.R
+++
@@ -1,9 +0,0 @@
-## 3  
-d<-read.csv("pgatour2006.csv",header = T);head(d)
-dd<-d[-c(1,2,4,11)]
-head(dd)
-### a  
-pairs(dd,upper.panel = NULL)
-ddd<-dd
-ddd$PrizeMoney<-log(dd$PrizeMoney)
-pairs(ddd, upper.panel = NULL)
---
+++ Regression601/hwk6/hwk6.R
@@ -0,0 +1,21 @@
+## 1
+
+### transformation
+### ridge
+### PCR 
+### PLS  
+
+## 2
+
+### transformation
+### ridge
+### PCR 
+### PLS  
+
+# 3. glm 
+
+# 4.logit 
+
+# 5.bootstrap procedure for 4
+
+# 6.poisson regression
